40359ece12d38071ae5029d7d59b778da6e26a06
zinmarmyint <zmar9251@gmail.com>
2022-06-29 12:37:22 +0630

    item-list

--- project/js/app.js
+++ project/js/app.js
@@ -14,3 +14,13 @@ function go(url){
 
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+$(".full-screen-btn").click(function(){
+    let current = $(this).closest(".card");
+    current.toggleClass("full-screen-card");
+    if(current.hasClass("full-screen-card")){
+        $(this).html(`<i class="feather-minimize-2"></i>`);
+    }else{
+        $(this).html(`<i class="feather-maximize-2"></i>`)
+    }
+});
